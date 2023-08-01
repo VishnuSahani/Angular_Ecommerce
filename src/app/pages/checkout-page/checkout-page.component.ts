e80acf4e463b8a85456d98de696f09746865b49c
Vishnu Sahani <vishnu83550@gmail.com>
2023-08-01 18:47:16 +0530

    add all state
    Signed-off-by: Vishnu Sahani <vishnu83550@gmail.com>

--- src/app/pages/checkout-page/checkout-page.component.ts
+++ src/app/pages/checkout-page/checkout-page.component.ts
@@ -33,6 +33,36 @@ export class CheckoutPageComponent implements OnInit {
 
   mainUserData= {};
 
+  stateArr = [ "Andhra Pradesh","Arunachal Pradesh","Assam",  "Bihar","Chhattisgarh","Goa","Gujarat","Haryana",
+  "Himachal Pradesh",
+  "Jammu and Kashmir",
+  "Jharkhand",
+  "Karnataka",
+  "Kerala",
+  "Madhya Pradesh",
+  "Maharashtra",
+  "Manipur",
+  "Meghalaya",
+  "Mizoram",
+  "Nagaland",
+  "Odisha",
+  "Punjab",
+  "Rajasthan",
+  "Sikkim",
+  "Tamil Nadu",
+  "Telangana",
+  "Tripura",
+  "Uttarakhand",
+  "Uttar Pradesh",
+  "West Bengal",
+  "Andaman and Nicobar Islands",
+  "Chandigarh",
+  "Dadra and Nagar Haveli",
+  "Daman and Diu",
+  "Delhi",
+  "Lakshadweep",
+  "Puducherry"]
+
   ngOnInit() {
     this.getUserData();
     this.calCulateValue();
--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -15,6 +15,7 @@ export class UserProfileComponent implements OnInit {
   userDetails:FormGroup;
   base64textString:any="";
   imageShow:any="";
+  // imageShow:any="assets/img/profilePhoto/Profile-Male.png";
 
   constructor(
     public mainService:MainServiceService,
@@ -25,6 +26,43 @@ export class UserProfileComponent implements OnInit {
 
 
   ) { }
+
+  stateArr = [ "Andhra Pradesh",
+                "Arunachal Pradesh",
+                "Assam",
+                "Bihar",
+                "Chhattisgarh",
+                "Goa",
+                "Gujarat",
+                "Haryana",
+                "Himachal Pradesh",
+                "Jammu and Kashmir",
+                "Jharkhand",
+                "Karnataka",
+                "Kerala",
+                "Madhya Pradesh",
+                "Maharashtra",
+                "Manipur",
+                "Meghalaya",
+                "Mizoram",
+                "Nagaland",
+                "Odisha",
+                "Punjab",
+                "Rajasthan",
+                "Sikkim",
+                "Tamil Nadu",
+                "Telangana",
+                "Tripura",
+                "Uttarakhand",
+                "Uttar Pradesh",
+                "West Bengal",
+                "Andaman and Nicobar Islands",
+                "Chandigarh",
+                "Dadra and Nagar Haveli",
+                "Daman and Diu",
+                "Delhi",
+                "Lakshadweep",
+                "Puducherry"]
 
   ngOnInit() {
     this.newForm();
